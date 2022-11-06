689f9b84e0ade2702add675d468c7bd5293a46c0
Manas Muda <manas@hitwicket.com>
2022-11-06 13:24:31 +0530

    Player Environment controller added

---
+++ Assets/Scripts/Player/PlayerEnvController.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+namespace EscapeMaze.Game.Player {
+    public class PlayerEnvController : MonoBehaviour {
+        public GameObject player;
+        private Transform _playerTransform;
+
+        private void Awake() {
+            _playerTransform = player.transform;
+        }
+
+        public void SetPlayerPosition(Vector3 position) {
+            _playerTransform.position = new Vector3(position.x, _playerTransform.position.y, position.z);
+        }
+    }
+}
+
+
